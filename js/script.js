eeb6c738b2e6b1731b7e8d5bd9c70971e65e63e2
Rexdown <shkabura2014@ya.ru>
2021-09-18 19:30:34 +0400

    ready project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const tabs = require('./modules/tabs'),
+          timer = require('./modules/timer'),
+          modal = require('./modules/modal'),
+          forms = require('./modules/forms'),
+          cards = require('./modules/cards'),
+          slider = require('./modules/slider'),
+          calc = require('./modules/calc');
+          
+    tabs();
+    timer();
+    modal();
+    forms();
+    cards();
+    slider();
+    calc();
+});
